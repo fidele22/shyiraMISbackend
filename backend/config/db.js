a1f7b3c7e9173b9510f9f620170e2415f555771d
fidele22 <fidelehirwa2@gmail.com>
2024-11-07 15:57:05 +0200

    dbconfig

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,47 @@
 const mongoose = require('mongoose');
+const Grid = require('gridfs-stream');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/shyiradb', { 
-      // You can add options here if needed
+    await mongoose.connect('mongodb+srv://fidelehirwa23:K9Z0AKzw5EHhJ6nH@cluster0.pcjespr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+      //useNewUrlParser: true,
+     // useUnifiedTopology: true,
     });
-    
-    console.log('MongoDB connected');
-  } catch (error) {
 
+    console.log('MongoDB connected');
+
+    // Initialize GridFS
+    const conn = mongoose.connection;
+    const gfs = Grid(conn.db, mongoose.mongo);
+    gfs.collection('uploads'); // Collection to store files
+
+    module.exports.gfs = gfs;
+
+  } catch (error) {
     console.error('MongoDB connection error:', error);
     process.exit(1);
-    
   }
 };
 
 module.exports = connectDB;
+
+
+
+// const mongoose = require('mongoose');
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect('mongodb://localhost:27017/shyiradb', { 
+//       // You can add options here if needed
+//     });
+    
+//     console.log('MongoDB connected');
+//   } catch (error) {
+
+//     console.error('MongoDB connection error:', error);
+//     process.exit(1);
+    
+//   }
+// };
+
+// module.exports = connectDB;
